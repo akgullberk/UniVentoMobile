363cd36964b30acb38f6d67fa1e400e80ff8c757
Berk Akgül <berkak_gul51@hotmail.com>
2025-03-04 21:33:40 +0300

    routes entegrasyonu yapıldı

---
+++ src/routes/Navigation.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+    );
+}
+
+export default Navigation
