d7ee5c333f52c6ad22a00045d0e4f41de307f632
Berk Akgül <berkak_gul51@hotmail.com>
2025-03-05 17:06:24 +0300

    topbar eklendi

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,7 @@
-import {Text, View, SafeAreaView } from 'react-native'
+import {Text, View, SafeAreaView, TextInput } from 'react-native'
 import React from 'react'
 import styles from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const Home = () => {
   return (
@@ -11,7 +12,21 @@ const Home = () => {
           <View style={styles.menuLine} />
           <View style={styles.menuLine} />
         </View>
-        <Text style={styles.title}>UniVento</Text>
+        
+        <View style={styles.centerContainer}>
+          <Text style={styles.title}>UniVento</Text>
+        </View>
+
+        <Icon name="notifications" size={24} color="#fff" />
+
+        <View style={styles.searchContainer}>
+          <TextInput 
+            style={styles.searchInput}
+            placeholder="Etkinlik ara..."
+            placeholderTextColor="#666"
+          />
+          <Icon name="search" size={28} color="#666" />
+        </View>
       </View>
     </SafeAreaView>
   )
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -15,12 +15,27 @@ export default StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 15,
-        
+        justifyContent: 'space-between',
+        zIndex: 1,
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
     },
     menuIcon: {
         width: 30,
         height: 20,
         justifyContent: 'space-between'
+    },
+    centerContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        alignItems: 'center'
     },
     menuLine: {
         width: '100%',
@@ -30,7 +45,32 @@ export default StyleSheet.create({
     title: {
         color: '#fff',
         fontSize: 20,
-        fontWeight: 'bold',
-        marginLeft: 20
+        fontWeight: 'bold'
+    },
+    rightIcon: {
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    searchContainer: {
+        position: 'absolute',
+        top: 75,
+        left: 15,
+        right: 15,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 25,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        elevation:15,
+        
+    },
+    searchInput: {
+        flex: 1,
+        marginLeft: 12,
+        fontSize: 18,
+        color: '#333'
     }
 })
