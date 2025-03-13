31c07bd4b0682ad711ee2d46f7f4cb4e76ff0d56
Berk Akgül <berkak_gul51@hotmail.com>
2025-03-13 15:09:04 +0300

    kulüp seçme ekranı tasarımı oluşturuldu

--- src/screens/KulupSecme/KulupSecme.js
+++ src/screens/KulupSecme/KulupSecme.js
@@ -1,12 +1,27 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { useNavigation } from '@react-navigation/native'
 
 const KulupSecme = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Kulüp Seçme</Text>
+      <View style={styles.searchContainer}>
+        <TextInput 
+          style={styles.searchInput}
+          placeholder="Kulüp ara"
+          placeholderTextColor="#666"
+        />
+        <Icon name="search" size={28} color="#666" style={styles.searchIcon} />
+      </View>
+
+      <View style={styles.overlay}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.closeButton}>
+          <Text style={styles.closeButtonText}>Kapat</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
--- src/screens/KulupSecme/styles.js
+++ src/screens/KulupSecme/styles.js
@@ -3,7 +3,59 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: '#88141c'
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 25,
+        margin: 15,
+        marginTop: 30,
+        paddingHorizontal: 15,
+        height: 50,
+    },
+    searchInput: {
+        flex: 1,
+        marginLeft: 10,
+        fontSize: 18,
+        color: '#333',
+        textAlignVertical: 'center',
+    },
+    searchIcon: {
+        marginLeft: 10
+    },
+    overlay: {
+        position: 'absolute',
+        bottom: 10,
+        left: 0,
+        right: 0,
+        height: 60,
+        backgroundColor: '#88141c',
+        borderTopWidth: 1,
+        borderTopColor: 'rgba(255, 255, 255, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+    },
+    closeButton: {
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 25,
+        width: '95%',
+        alignItems: 'center',
+    },
+    closeButtonText: {
+        color: '#808080',
+        fontSize: 16,
+        fontWeight: 'bold'
     },
     header: {
         width: '100%',
